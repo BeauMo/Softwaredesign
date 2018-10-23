5c58ec3a7d8f442ff4a3807c6e0570ad0e70497c
BeauMo <43986314+BeauMo@users.noreply.github.com>
2018-10-23 23:33:12 +0200

    v1.1

--- Program.cs
+++ Program.cs
@@ -6,34 +6,29 @@ namespace Softwaredesign
     {
         static void Main(string[] args)
         {
+            String romanNumber = "";
+
             int zahl = Convert.ToInt32(args[0]);
             String[] einer = {"","I","II","III","IV","V","VI","VII","VIII","IX"}; 
             String[] zehner = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
             String[] hunderter = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-            String romanNumber = "";
-            int save = 0;
-            int zahlSave = zahl;
+
+            int intEiner = zahl%10;
+            int intZehner = ((zahl-intEiner)%100)/10;
+            int intHunderter = ((zahl-intEiner-intZehner)%100)/100;
 
             if( zahl < 0 && zahl > 999){
                 Console.Write("Error");
             }
-            String sNumber = args[0].ToString();
-            int number = Convert.ToInt32(sNumber[0]);
             
             if(zahl >= 100){
-                save = zahl%100;
-                zahlSave = (zahlSave - save)/100;
-                romanNumber = romanNumber + hunderter[zahlSave].ToString();
+                romanNumber = romanNumber + hunderter[intHunderter].ToString();
             }
             if(zahl >= 10){
-                int a = save%10;
-                zahlSave = (save-a)/10;
-                romanNumber = romanNumber + zehner[zahlSave].ToString();
-                zahlSave = 0;
+                romanNumber = romanNumber + zehner[intZehner].ToString();
             }
             if(zahl >= 0){
-                save = zahl%10;
-                romanNumber = romanNumber + einer[save].ToString();
+                romanNumber = romanNumber + einer[intEiner].ToString();
             }
 
             Console.Write(romanNumber);
