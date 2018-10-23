c114091b67692c27cca77a3d04c349989317e1e2
BeauMo <43986314+BeauMo@users.noreply.github.com>
2018-10-23 11:45:27 +0200

    new

--- Program.cs
+++ Program.cs
@@ -4,13 +4,29 @@ namespace Softwaredesign
 {
     class Program
     {
-        static void Main(int[] args)
+        static void Main(string[] args)
         {
-            if( args[0] < 0 && args[0] > 999){
+            int zahl = Convert.ToInt32(args[0]);
+            String[] einer = {"","I","II","III","IV","V","VI","VII","VIII","IX"}; 
+            String[] zehner = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+            String[] hunderter = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+            Array[] arrays = {einer,zehner,hunderter};
+            String romanNumber = "";
+            int i;
+            int mod = 10;
+
+            if( zahl < 0 && zahl > 999){
                 Console.Write("Error");
             }
             String sNumber = args[0].ToString();
             int number = Convert.ToInt32(sNumber[0]);
+
+            for(int a = 0; a < sNumber.Length; a++){
+                i = mod%zahl;
+                romanNumber = romanNumber + einer[i].ToString();
+                i=i+10;
+            }
+            Console.Write(romanNumber);
         }
     }
 }
