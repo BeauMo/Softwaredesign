633df70f524ddfaabd1e3747af0f3ad56561e57f
BeauMo <43986314+BeauMo@users.noreply.github.com>
2018-11-18 13:20:32 +0100

    A5

---
+++ A5 by me/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace A5_by_me
+{
+    class Program
+    {
+        static StringBuilder wordsBackwardsSB = new StringBuilder();
+        static void Main(string[] args)
+        {
+            string sentence = "Die Maus frisst Käse";
+            string[] words = sentence.Split(" ");
+
+            Console.WriteLine(sentence);
+            Console.WriteLine(backwardsPerLetter(sentence));
+            backwardsPerWord(words);
+            Console.WriteLine(wordsBackwardsSB);
+        }
+
+        static string backwardsPerLetter(string stringToTurn){
+            StringBuilder letters = new StringBuilder();
+            for(int i = stringToTurn.Length-1; i >= 0; i--){
+                letters.Append(stringToTurn[i]);
+            }
+            return letters.ToString();
+        }
+
+        static void backwardsPerWord(string[] stringToExchange){
+            StringBuilder words = new StringBuilder();
+            for(int i = stringToExchange.Length-1; i >= 0; i--){
+                words.Append(stringToExchange[i] + " ");
+                wordsBackwardsSB.Append(backwardsPerLetter(stringToExchange[stringToExchange.Length-1-i] + " "));
+            }
+            wordsBackwardsSB.Remove(0,1);
+            Console.WriteLine(words.ToString());
+        }
+    }
+}
---
+++ A5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace A5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = "Die Maus frisst Käse";
+
+            Console.WriteLine(input);
+            Console.WriteLine(reverseString(input));
+            Console.WriteLine(reverseSentence(input));
+        }
+
+        static String reverseString(String input){
+            char[] reverseInput = input.ToCharArray();
+            Array.Reverse(reverseInput);
+            String returnString = new String(reverseInput);
+            return returnString;
+        }
+
+        static String reverseSentence(String input){
+            string[] splittedInput = input.Split(" ");
+            Array.Reverse(splittedInput);
+            String reversedSentence = String.Join(separator: " ", value: splittedInput);
+            return reversedSentence;
+        }
+
+        static String reverseWord(String input){
+            string[] inputArray = input.Split(" ");
+            StringBuilder reversedWord = null;
+
+            for(int i = 0; i < inputArray.Length; i++){
+                reversedWord.Append(Reverse(inputArray[i]));
+                reversedWord.Append(" ");
+            }
+            return reversedWord.ToString();
+        }
+    }
+}
